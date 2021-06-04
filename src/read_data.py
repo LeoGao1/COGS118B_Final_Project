1c2fa796e32a5d6cffe3730a838d843ef5837a40
LeoGao1 <weg021@ucsd.edu>
2021-06-03 18:00:19 -0700

    update

---
+++ src/read_data.py
@@ -0,0 +1,19 @@
+
+import numpy as np
+
+def read_exp(exp_num):
+    exp_num = 1
+    name1 = '../data/x_train_'+str(exp_num)+'.npy'
+    name2 = '../data/y_train_'+str(exp_num)+'.npy'
+    name3 = '../data/x_test_'+str(exp_num)+'.npy'
+    name4 = '../data/y_test_'+str(exp_num)+'.npy'
+
+    x_train = np.load(name1)
+    y_train = np.load(name2)
+    x_test = np.load(name3)
+    y_test = np.load(name4)
+
+    print('Experiment: ' + str(exp_num))
+    print('The data are x_train,y_train,x_test,y_test')
+
+    return x_train,y_train,x_test,y_test
